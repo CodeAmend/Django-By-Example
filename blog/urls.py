3307fc22106cb7a6060a16b7b99082c48f871ba4
Michael Bruce <codeamend@gmail.com>
2016-06-27 14:33:31 -0400

    Pagination refactored into django.views.generic ListView lost functionality of exceptions for EmptyPage and PageNotAnInteger

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .views import PostListView
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    # url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
