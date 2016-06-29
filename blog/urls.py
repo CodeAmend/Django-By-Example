c05187a391bdce245b31a494f606c895ecbc17e5
Michael Bruce <codeamend@gmail.com>
2016-06-29 14:18:54 -0400

    tag system works. can add edit and remove also display by tag slug

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from . import views
-from .views import PostListView
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<post_id>[0-9]+)/share/$', views.post_share, name='post_share'),
-    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
+    url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag')
 ]
